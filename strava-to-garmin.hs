4117705c8fe8445a950a3930111da4f25bdc028e
Konsta Purtsi <konsta@arado.fi>
2024-03-11 20:11:12 +0200

    Parse datetimes

--- strava-to-garmin.hs
+++ strava-to-garmin.hs
@@ -3,19 +3,22 @@ import Prelude
 import System.Environment
 import System.Exit
 import Data.Either
+import Data.List
+import Data.Time
+import Data.Time.Format.ISO8601
 import Text.XML.Light
 
 -- Parse relevant data from a .tcx file exported from Strava for Garmin import.
 -- Assumes the .tcx is in Technogym's format, 1 lap only.
 
-main = getArgs >>= parseArgs >>= readFile >>= (print . parseFile)
+main = getArgs >>= parseArgs >>= readFile >>= (report . parseFile)
 
 parseArgs [f] = pure f
 parseArgs []  = putStrLn "Enter the filename of the input."                         >> exitFailure
 parseArgs _   = putStrLn "Invalid arguments, input only the filename of the input." >> exitFailure
 
 data Activity = Activity
-  { startTime :: String
+  { startTime :: UTCTime
   , totalTimeSeconds :: Int
   , totalDistanceMeters :: Int
   , calories :: Int
@@ -23,13 +26,14 @@ data Activity = Activity
   } deriving Show
 
 data TrackPoint = TrackPoint
-  { time :: String
+  { time :: UTCTime
   , distanceMeters :: Float
   , cadence :: Int
   , watts :: Int
   , heartRateBpm :: Int
   } deriving Show
 
+parseFile :: String -> Maybe Activity
 parseFile source = do
   let
     garmin_qURI = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
@@ -39,16 +43,16 @@ parseFile source = do
 
     fnd n = findElement (QName n (Just garmin_qURI) Nothing)
     fndActivity n = findElement (QName n (Just activity_qURI) Nothing)
-    fndInt n l = do
-      el <- fnd n l
-      pure $ intContent el
+    fndInt n l = intContent <$> fnd n l
+
+    parseISO = formatParseM (iso8601Format @UTCTime)
 
     contents = parseXML source
     activity = head $ (findElements (QName "Activity" (Just garmin_qURI) Nothing)) `concatMap` onlyElems contents
 
   lap <- fnd "Lap" activity
 
-  startTime <- findAttr (QName "StartTime" Nothing Nothing) lap
+  startTime <- findAttr (QName "StartTime" Nothing Nothing) lap >>= parseISO
 
   totalTimeSeconds <- fndInt "TotalTimeSeconds" lap
   totalDistanceMeters <- fndInt "DistanceMeters" lap
@@ -57,7 +61,7 @@ parseFile source = do
 
   let
     parseTrackPoint t = do
-      time <- strContent <$> fnd "Time" t
+      time <- parseISO =<< strContent <$> fnd "Time" t
       distanceMeters <- read . strContent <$> fnd "DistanceMeters" t
       cadence <- fndInt "Cadence" t
       watts <- intContent <$> (fndActivity "TPX" t >>= fndActivity "Watts")
@@ -68,6 +72,9 @@ parseFile source = do
 
   pure Activity { startTime, totalTimeSeconds, totalDistanceMeters, calories, trackpoints }
 
-report (Left err) = putStrLn ("Error: Failed to parse file, ") >> exitFailure
-report (Right res) = putStrLn res >> exitSuccess
+report Nothing = print "Failed to parse."
+report (Just Activity { trackpoints }) = putStrLn $ intercalate "\n" $ map (\t -> show (time t) ++ " " ++ show (watts t)) trackpoints
+
+-- report (Left err) = putStrLn ("Error: Failed to parse file, ") >> exitFailure
+-- report (Right res) = putStrLn res >> exitSuccess
 
